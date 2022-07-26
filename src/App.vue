274098f34fd64a7c7fcacf5f369ebdcf0d7ce436
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2022-07-26 14:02:48 +0300

    добавлена разметка для селекта

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
-  <Form />
-  <Cards />
+  <div class="products">
+    <h1 class="products__title">Добавление товара</h1>
+    <Select />
+    <!-- <Form />
+    <Cards /> -->
+  </div>
 </template>
+
 <script>
-import Form from "@/components/Form.vue";
-import Cards from "@/components/Cards.vue";
+import Select from "@/components/Select.vue";
+//import Form from "@/components/Form.vue";
+//import Cards from "@/components/Cards.vue";
 export default {
-  components: { Form, Cards },
+  //components: { Form, Cards },
+  components: { Select },
   name: "App",
 };
 </script>
 
 <style lang="scss" scoped>
 #app {
-  //background: $color-purple;
+  background: rgba(255, 254, 251, 0.8);
 }
 
-.main {
-  // background: $color-purple;
-  // color: $color-white;
-  // max-width: 1920px;
-  // min-width: 360px;
-  // min-height: 100vh;
-  // margin: 0 auto;
+.products {
+  color: $color-black;
+  max-width: 1440px;
+  min-width: 360px;
+  min-height: 100vh;
+  margin: 0 auto;
+  padding: 32px;
+
+  &__title {
+    margin: 0;
+  }
 }
 </style>
---
+++ src/components/Select.vue
@@ -0,0 +1,19 @@
+<template>
+  <select class="select">
+    <option>По умолчанию</option>
+    <option>По цене min (от меньшего к большему)</option>
+    <option>По цене max (от большего к меньшему)</option>
+    <option>По наименованию</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "SelectBlock",
+};
+</script>
+
+<style lang="scss" scoped>
+.select {
+}
+</style>
