2fc85eb2a274a49c2d28ea29e00cb8d818201b48
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2022-07-28 13:47:13 +0300

    выполнение заданий 2-го уровня

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./main.scss";
 import Maska from "maska";
-import store from "../store";
+import store from "../store/index";
 
 createApp(App).use(Maska).use(store).mount("#app");
